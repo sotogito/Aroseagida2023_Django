31bdb8ae42d8dd21a5eb612e24e38ac7159f5e7d
sotogito <108535526+sotogito@users.noreply.github.com>
2023-08-22 15:26:33 +0900

    unity로 사용자 나이 보내기 구현 마침

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from accounts.forms import SignupForm,AddUserProfileForm
-from datetime import date
 from .models import AddUserProfile
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from datetime import date
+from datetime import datetime
 
 
 def signup(request):
@@ -29,13 +30,15 @@ def signup(request):
 
 @api_view(['GET'])
 def get_user_age(request):
-    user = AddUserProfile.objects.filter(id = 13).first()
-    a = user.birthdate.year
+    user = AddUserProfile.objects.filter(id = 13).first() #임의의 사용자를 지정
+    user_birthdate = user.birthdate
+    now_date = datetime.now().date()
+    user_age = now_date.year - user_birthdate.year - ((now_date.month, now_date.day) < (user_birthdate.month, user_birthdate.day))
+    user_age_num = user_age // 10
 
-    b = 2
     context = {
-        'a' : a,
-        'b' : b
+        'user_age' : user_age, #나이
+        'user_age_num' : user_age_num #나이대를 계산할 값
     }
     return Response(context)
 
